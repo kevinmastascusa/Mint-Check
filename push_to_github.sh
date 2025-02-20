7ba9f1150c2bdf9bab8cdd7dc6325647cfd32e6c
Kevin Zhou Mastascusa <83515571+kevinmastascusa@users.noreply.github.com>
2025-02-20 16:42:14 -0500

    Initial commit excluding large file

--- push_to_github.sh
+++ push_to_github.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 git init
+# Add the large file to .gitignore
+echo "data/GitHubDesktopSetup-x64.exe" >> .gitignore
+git add .gitignore
 git add .
-git commit -m "Initial commit"
+# Remove the large file from the index if it was added
+git rm --cached data/GitHubDesktopSetup-x64.exe
+git commit -m "Initial commit excluding large file"
 git remote add origin https://github.com/kevinmastascusa/Mint-Check.git
 git push -u origin master
 
